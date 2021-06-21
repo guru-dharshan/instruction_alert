b3edd6c60302b921a5062a96c3e53befdbd02032
guru-dharshan <dharshangd1906@gmail.com>
2021-06-21 23:41:40 +0530

    Initial commit

---
+++ app/src/main/java/com/devgd/swipegesture/MainActivity.java
@@ -0,0 +1,62 @@
+package com.devgd.swipegesture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showinstruction();
+
+    }
+
+    private void showinstruction() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.instruction);
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        View view1 = dialog.findViewById(R.id.view1);
+        View view2 = dialog.findViewById(R.id.view2);
+        View view3 = dialog.findViewById(R.id.view3);
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view1.setVisibility(View.INVISIBLE);
+                view2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view2.setVisibility(View.INVISIBLE);
+                view3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view3.setVisibility(View.INVISIBLE);
+                dialog.cancel();
+            }
+        });
+
+    }
+
+}
